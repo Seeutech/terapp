37a7309caf5502509ea7337a724d95fa5b093757
Seeutech <118717438+Seeutech@users.noreply.github.com>
2024-03-17 09:33:24 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,15 +1,1 @@
-const sendFile = async (item, ctx) => {
-  if (item) {
-    try {
-      await ctx.replyWithDocument(item);
-    } catch (e) {
-      ctx.replyWithMarkdown(
-        `error ${e.message}\n\n Try to download from [here](${item})\n\nThe file maybe Large*`,
-      );
-    }
-  }
-};
 
-module.exports = {
-  sendFile,
-};
